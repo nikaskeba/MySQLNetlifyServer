229fcb165a856bdb1824280c377b63545a2ec806
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-09-24 23:35:22 -0700

    headers fix

--- netlify-functions/save-biography.js
+++ netlify-functions/save-biography.js
@@ -2,25 +2,23 @@ const mysql = require('mysql2/promise');
 
 // MySQL connection configuration
 const dbConfig = {
-  host: 'https://162.240.77.218/',
+  host: '162.240.77.218',
   user: 'skeba_netlify',
   password: process.env.MYSQL_PASSWORD,
   database: 'skeba_netlify'
 };
- let HEADERS = {
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-  'Content-Type': 'application/json', //optional
+const headers = {
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Max-Age': '8640'
-}
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
+  'Content-Type': 'application/json',
+  'Access-Control-Max-Age': '8640',
+  'Vary': 'Origin'
+};
 
-//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-
-HEADERS['Access-Control-Allow-Origin'] = '*'
-HEADERS['Vary'] = 'Origin'
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { statusCode: 405, headers, body: 'Method Not Allowed' };
   }
 
   const data = JSON.parse(event.body);
@@ -30,16 +28,16 @@ exports.handler = async (event, context) => {
     const connection = await mysql.createConnection(dbConfig);
     await connection.execute('INSERT INTO user_biography (biography) VALUES (?)', [biography]);
     connection.end();
-   return {
+    return {
       statusCode: 200,
-      HEADERS,
+      headers,
       body: JSON.stringify({ message: 'Biography saved successfully' })
     };
   } catch (error) {
     console.error(error);
-     return {
+    return {
       statusCode: 500,
-       HEADERS,
+      headers,
       body: JSON.stringify({ error: 'Failed to save biography' })
     };
   }
